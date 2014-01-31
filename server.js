e2608f3a933d2eb20a41a05e38d08f7cf05e58cc
kasparsl <kaspars.leinis@gmail.com>
2014-01-31 23:18:18 +0200

    node.js express URL routing test

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req,res){
-	res.writeHead(200,{'Content-type':'text/plain'});
-	res.end('Hello world!');
-	}).listen(1337, '127.0.0.1');
-console.log('Webserver has started');
-
